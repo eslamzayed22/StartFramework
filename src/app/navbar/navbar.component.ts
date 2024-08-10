5a9677d9fc074820dcb628ae6c3030dd15a568ba
eslamzayed22 <eslamzayed765@gmail.com>
2024-08-10 11:56:26 +0300

    second

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -16,14 +16,10 @@ export class NavbarComponent {
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const verticalOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-
-    // Check if scrolled down more than 100px
     if (verticalOffset > 100) {
-      // Remove py-3 class from navbar
       this.renderer.removeClass(document.querySelector('nav'), 'py-3' );
       this.isNavbarExpanded = false;
     } else {
-      // Add py-3 class back to navbar
       this.renderer.addClass(document.querySelector('nav'), 'py-3');
       this.isNavbarExpanded = true;
     }
